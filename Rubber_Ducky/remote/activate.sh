6f94c7ecd9b9030aeba985abd91b34ad64869cc0
panik-144 <paul.n.b@outlook.de>
2025-11-21 17:10:53 +0100

    super commit

---
+++ Rubber_Ducky/remote/activate.sh
@@ -0,0 +1,6 @@
+SELF_DESTRUCT
+# Dieser Befehl signalisiert dem Agenten, sich selbst zu löschen.
+# Er entfernt:
+# 1. ~/.hidden_updater.sh
+# 2. ~/Library/LaunchAgents/com.apple.systemupdate.plist
+# 3. Den laufenden Prozess
