724d462c5e58407035e2dbdb3647d3a670bd48e8
panik-144 <paul.n.b@outlook.de>
2025-11-21 14:49:00 +0100

    super commit

---
+++ Rubber_Ducky/remote/activate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# macOS Payload (activate.sh)
+# Dieser Code wird ausgeführt, sobald der Ducky "scharf" geschaltet ist.
+
+# Beispiel: Sprachausgabe (Gruselig)
+#say "I am inside your computer."
+
+# Beispiel: Öffnet den Rechner
+open -a Calculator
+
+# Beispiel: Öffnet eine Webseite
+#open "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
